5b9076191dfc665ec9b28fdcde0e645697a5b6fa
Krisztiaan <krisz011@gmail.com>
2020-06-24 17:39:35 +0200

    Initial Commit

---
+++ ConnatixTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ConnatixTest
+//
+//  Created by Krisztian Ferencz on 2020. 06. 24..
+//  Copyright © 2020. Krisztian Ferencz. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ConnatixTestTests/ConnatixTestTests.swift
@@ -0,0 +1,34 @@
+//
+//  ConnatixTestTests.swift
+//  ConnatixTestTests
+//
+//  Created by Krisztian Ferencz on 2020. 06. 24..
+//  Copyright © 2020. Krisztian Ferencz. All rights reserved.
+//
+
+import XCTest
+@testable import ConnatixTest
+
+class ConnatixTestTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
